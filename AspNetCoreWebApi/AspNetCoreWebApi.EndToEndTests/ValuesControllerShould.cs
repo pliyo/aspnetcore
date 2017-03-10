63b0e74c7e2667a7f57b8f35211e5ca91ad88b8e
Juanjo Cerezuela <Juanjo.Cerezuela@asos.com>
2017-03-10 17:13:29 +0000

    Adding NUnit

--- AspNetCoreWebApi/AspNetCoreWebApi.EndToEndTests/ValuesControllerShould.cs
+++ AspNetCoreWebApi/AspNetCoreWebApi.EndToEndTests/ValuesControllerShould.cs
@@ -1,14 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
 
 namespace AspNetCoreWebApi.EndToEndTests
 {
-    public class Class1
+    [TestFixture]
+    public class ValuesControllerShould
     {
-        public Class1()
+        private TestServer _server;
+        private HttpClient _client;
+
+        [SetUp]
+        public void SetUp()
         {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Test]
+        public async Task ReturnValue()
+        {
+            var response = await _client.GetAsync("api/values/5");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.That("value", Is.EqualTo(responseString));
         }
     }
 }
