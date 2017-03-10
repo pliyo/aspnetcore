8b18a7c49075b800cbb1df1c8912a12e896ae176
Juanjo Cerezuela <Juanjo.Cerezuela@asos.com>
2017-03-10 17:15:46 +0000

    Adding UnitTests

---
+++ AspNetCoreWebApi/AspNetCoreWebApi.UnitTests/ValuesControllerShould.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebApi.UnitTests
+{
+    [TestFixture]
+    public class ValuesControllerShould
+    {
+        [Test]
+        public void ValuesController()
+        {
+            Assert.True(true);
+        }
+    }
+}
