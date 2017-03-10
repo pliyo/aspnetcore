d27051433257b95d8543ce4ecdee0d60d361f536
Juanjo Cerezuela <Juanjo.Cerezuela@asos.com>
2017-03-10 16:56:00 +0000

    Skeleton for WebApi

---
+++ AspNetCoreWebApi/AspNetCoreWebApi.EndToEndTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ASOS.com")]
+[assembly: AssemblyProduct("AspNetCoreWebApi.EndToEndTests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a70f2590-d7e5-4e45-8150-e62ce80d98fd")]
